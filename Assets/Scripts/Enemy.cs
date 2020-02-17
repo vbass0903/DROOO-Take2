a20e4c165d5bd032e8797b36aafef5f8fc2c484f
zackloch <zacharylochmueller2017@u.northwestern.edu>
2020-02-17 01:29:45 -0600

    Enemy Waves and Oxygen Depletion 1

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private GameObject en;
+    Rigidbody2D _rb;
     private GameObject sub;
     public GameObject bar;
     public float hitpoints;
     public string nam;
+    public Vector2 targetDirection;
+    float moveSpeed;
 
     public Enemy(int hp, string ID) 
     {
@@ -17,14 +19,16 @@ public class Enemy : MonoBehaviour
     }
     void Start()
     {
-
+        sub = GameObject.Find("Submarine");
+        _rb = GetComponent<Rigidbody2D>();
+        moveSpeed = Random.Range(2f, 3f);
     }
 
     void Update()
     {
         //en.transform.RotateAround(sub.transform.position, Vector3.back, 20f * Time.deltaTime);
         bar = GameObject.Find("OxygenBar");
-        transform.position = new Vector3(transform.position.x - 0.05f, transform.position.y, transform.position.z);
+        MoveEnemy();
     }
     private void OnTriggerEnter2D(Collider2D col)
     {
@@ -34,5 +38,14 @@ public class Enemy : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    //Test
+    private void MoveEnemy()
+    {
+        if (sub != null)
+        {
+            targetDirection = (sub.transform.position - transform.localPosition).normalized;
+            _rb.velocity = new Vector2(targetDirection.x * moveSpeed, targetDirection.y * moveSpeed);
+        }
+        else
+            _rb.velocity = Vector3.zero;
+    }
 }
---
+++ Assets/Scripts/SpawnerControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerControl : MonoBehaviour
+{
+
+    public Transform[] spawns;
+    public GameObject enemy;
+    int randomSpawn;
+    public static bool spawnAllowed;
+    public float SpawnEveryXSeconds = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnAllowed = true;
+        InvokeRepeating("EnemySpawn", 0f, SpawnEveryXSeconds);
+    }
+
+    void EnemySpawn()
+    {
+        if (spawnAllowed)
+        {
+            randomSpawn = Random.Range(0, spawns.Length);
+            Instantiate(enemy, spawns[randomSpawn].position, Quaternion.identity);
+        }
+    }
+    
+    void Update()
+    {
+        
+    }
+}
