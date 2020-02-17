6237e4da2f9a9bd5d671e4b2ed7dc7cf53690e29
vbass0903 <32740596+vbass0903@users.noreply.github.com>
2020-02-17 11:14:21 -0600

    Purifier Start

---
+++ Assets/Scripts/Purifier.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Purifier : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision);
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log(collision);
+    }
+
+}
