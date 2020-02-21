eed93d4e753bc1bc11e762670a990a4445f21ecd
vbass0903 <32740596+vbass0903@users.noreply.github.com>
2020-02-20 20:20:42 -0600

    Purifier Progress Bar
    Slight bug at the end, overlaps a little

---
+++ Assets/Scripts/purifierProgress.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class purifierProgress : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    GameObject purifier;
+    public float progress = 0f;
+    float offset = 0f;
+    float compareProgress = 1f;
+    float startScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startScale = transform.localScale.y;
+        purifier = transform.parent.gameObject;
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Debug.Log(purifier.GetComponent<Purifier>().startTime != 0f);
+        if (purifier.GetComponent<Purifier>().startTime != 0f)
+        {
+            offset = Time.time - purifier.GetComponent<Purifier>().startTime;
+            progress = offset / purifier.GetComponent<Purifier>().surviveTime;
+            compareProgress = 1 - progress;
+
+            Debug.Log(new Vector3(offset, progress, compareProgress));
+
+            transform.localScale = new Vector3(transform.localScale.x, startScale - progress, transform.localScale.z);
+        }
+    }
+}
