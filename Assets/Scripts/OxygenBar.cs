cd694484fba323c443afde1b6ac5b24a11f1cabe
zackloch <zacharylochmueller2017@u.northwestern.edu>
2020-03-09 02:06:29 -0500

    Pressure Bar Idea

--- Assets/Scripts/OxygenBar.cs
+++ Assets/Scripts/OxygenBar.cs
@@ -59,10 +59,10 @@ public class OxygenBar : MonoBehaviour
             Destroy(sub.gameObject); // need lose function somewhere*/
 
         }
-        if (oxygenLevel > 147f) // 147 is max localScale,  CHANGES IF SUB SCALE CHANGES
+        if (oxygenLevel > 161f) // 161 is max localScale,  CHANGES IF SUB SCALE CHANGES
         {
-            oxygenLevel = 147f;
-            bar.transform.localScale = new Vector2(147f, bar.transform.localScale.y);
+            oxygenLevel = 161f;
+            bar.transform.localScale = new Vector2(161f, bar.transform.localScale.y);
         }
     }
 }
