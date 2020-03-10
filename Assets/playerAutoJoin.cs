d7823d9d9498cef19ebe2c5183c39b16d65adbd4
vbass <32740596+vbass0903@users.noreply.github.com>
2020-03-10 02:01:52 -0500

    AutoSpawn WIP
    Autospawn spawns players based on number of connected controllers.
    
    WIP: Type Mismatch on Player Prefab - Player Input - Actions after running the scene once. This causes prefab to lose Player Input - Actions.

--- Assets/playerAutoJoin.cs
+++ Assets/playerAutoJoin.cs
@@ -6,5 +6,22 @@ using UnityEngine.InputSystem.Users;
 
 public class playerAutoJoin : MonoBehaviour
 {
+    private InputControlList<InputDevice> inputDevices;
+    public GameObject playerPrefab;
+    //public InputActions controllerScheme;
 
+    void Start()
+    {
+        Debug.Log(InputUser.GetUnpairedInputDevices());
+        inputDevices = InputUser.GetUnpairedInputDevices();
+        for (int i = 2; i < inputDevices.Count; i++)
+        {
+            var p1 = PlayerInput.Instantiate(playerPrefab, pairWithDevice: inputDevices[i]);
+        }
+    }
+
+    void Update()
+    {
+        Debug.Log(InputUser.GetUnpairedInputDevices());
+    }
 }
